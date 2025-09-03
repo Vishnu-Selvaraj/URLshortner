95908abb3f287dc458326a75d895860f487f5622
Vishnu-Selvaraj <vishnusraj1997@gmail.com>
2025-09-03 18:46:16 +0530

    Modified settings file

--- urlshortner_project/settings.py
+++ urlshortner_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG',False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST').split(',')
 
 
 # Application definition
